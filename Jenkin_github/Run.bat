57d105982dece79c5aa4609b84b3e995725902ce
dell <dell@DESKTOP-IEF8SQP>
2020-06-18 12:43:40 +0530

    this is my first commit

---
+++ Jenkin_github/Run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\dell\Jenkin_github
+mvn clean install 
---
+++ Jenkin_github/src/test/java/testCases/TC_001.java
@@ -0,0 +1,16 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class TC_001 {
+@Test 
+
+public void test1 () {
+	System.out.println("Hi i am Akash");
+}
+@Test 
+public void test2 () {
+	System.out.println("Hi i am Jayshri");
+}
+
+}
